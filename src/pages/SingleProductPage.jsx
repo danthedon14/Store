e4acc05a725d49528be44438758073eb5be5b260
Daniel <130522869+danthedon14@users.noreply.github.com>
2024-04-24 01:25:04 -0500

    Update SingleProductPage.jsx

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -26,15 +26,16 @@ const SingleProductPage = () => {
 
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`);
-  }, [fetchSingleProduct, id]);
-
+    // eslint-disable-next-line
+  }, [id]);
   useEffect(() => {
     if (error) {
       setTimeout(() => {
         navigate('/');
       }, 3000);
     }
-  }, [error, navigate]);
+    // eslint-disable-next-line
+  }, [error]);
 
   if (loading) {
     return <Loading />
