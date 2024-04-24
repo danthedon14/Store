c8d1f86f6b24bb17a6b1dd8b6e2e944d929c93f2
Daniel <130522869+danthedon14@users.noreply.github.com>
2024-04-24 00:37:27 -0500

    Update cart_context.js

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -7,7 +7,7 @@ import {
   CLEAR_CART,
   COUNT_CART_TOTALS,
 } from '../actions'
-import { ThemeConsumer } from 'styled-components'
+
 
 const getLocalStorage = () => {
   let cart = localStorage.getItem('fcart')
