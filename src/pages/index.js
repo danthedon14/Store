b1a356b5db0e02ce7a81aebdc3ffe21fe5cc6331
Daniel <dmehari147@gmail.com>
2024-04-23 23:55:38 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './HomePage'
+import Error from './ErrorPage'
+import AuthWrapper from './AuthWrapper'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Private from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+
+
+export { Home, Checkout, Error, Private, Products, SingleProduct, Cart, AuthWrapper, About }
